6ec1778d7ac7cc11f8bafc383b25ac0f7428cb8c
Aparna <aparnalekshmi23@gmail.com>
2020-10-29 11:11:26 +0530

    first commit

---
+++ AddTwoNum.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class AddTwoNum 
+{
+	public static void main(String[] args)
+	{
+		int a=2, b=3, sum;
+		sum = a+b;
+		System.out.println("Sum is :"+ sum);
+		
+	}
+
+}
---
+++ OddOrEven.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class OddOrEven 
+{
+	public static void main(String[] args)
+	{
+	    int num = 5;
+		if( num % 2 == 0)
+		
+	    	System.out.println("Given Number is Even!");
+		else
+		
+			System.out.println("Given Number is Odd");
+	}
+}
